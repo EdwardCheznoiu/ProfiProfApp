ed1ac83cfaf10c53ed6da3897f787438e7e3022e
EdwardCheznoiu <cheznoiu.edward@gmail.com>
2023-05-22 18:14:55 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import HeaderCarousel from './components/HeaderCarousel';
+function App()
+{
+  return (
+    <Navbar />
+  );
+}
+
+export default App;
